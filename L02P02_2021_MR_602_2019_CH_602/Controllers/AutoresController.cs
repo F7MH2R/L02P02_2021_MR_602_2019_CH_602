cfbb2bea38eac7be8da6fe81c937e342f7bc1d06
F7MH2R <skyicef7@gmail.com>
2024-04-20 15:14:02 -0600

    subiendo

--- L02P02_2021_MR_602_2019_CH_602/Controllers/AutoresController.cs
+++ L02P02_2021_MR_602_2019_CH_602/Controllers/AutoresController.cs
@@ -1,12 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using L02P02_2021_MR_602_2019_CH_602.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace L02P02_2021_MR_602_2019_CH_602.Controllers
 {
     public class AutoresController : Controller
     {
-        public IActionResult Index()
+        private readonly Context_Libreria _context;
+
+        public AutoresController(Context_Libreria context)
         {
-            return View();
+            _context = context;
         }
+
+        public async Task<IActionResult> Index()
+        {
+            var autores = await _context.autores.ToListAsync();
+            return View();
+
+        }
+
+
+
     }
 }
