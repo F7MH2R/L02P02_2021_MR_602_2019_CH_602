da61d653da2da2cfe16345accda3cba5e8f00efa
CarlosCastaneda04 <54734121+CarlosCastaneda04@users.noreply.github.com>
2024-04-20 15:49:49 -0600

    ahora si

--- L02P02_2021_MR_602_2019_CH_602/Controllers/AutoresController.cs
+++ L02P02_2021_MR_602_2019_CH_602/Controllers/AutoresController.cs
@@ -1,8 +1,6 @@
 ﻿using L02P02_2021_MR_602_2019_CH_602.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using System.Linq;
-
 
 namespace L02P02_2021_MR_602_2019_CH_602.Controllers
 {
@@ -10,16 +8,17 @@ namespace L02P02_2021_MR_602_2019_CH_602.Controllers
     {
         private readonly Context_Libreria _context;
 
+        public AutoresController(Context_Libreria context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             var autores = _context.autores.ToList(); // Recuperar todos los autores desde la base de datos
 
             return View(autores); // Pasar los autores a la vista
-
         }
-
-
-
     }
 }
 
