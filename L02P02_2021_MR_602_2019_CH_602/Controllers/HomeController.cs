c2cf0f39110dfd7e8b7dbbbd031f020a6917ac83
F7MH2R <skyicef7@gmail.com>
2024-04-20 12:38:57 -0600

    Add project files.

---
+++ L02P02_2021_MR_602_2019_CH_602/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using L02P02_2021_MR_602_2019_CH_602.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace L02P02_2021_MR_602_2019_CH_602.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ L02P02_2021_MR_602_2019_CH_602/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02P02_2021_MR_602_2019_CH_602.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
